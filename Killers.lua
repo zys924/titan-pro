c1aa60e84afcf752d3bfa9a8fbe44b4d8f86d748
zys924 <zys924@188.com>
2017-04-20 23:46:16 +1000

    Update Killers.lua

--- Killers.lua
+++ Killers.lua
@@ -152,7 +152,7 @@ PriestKiller = {
                 if (not shadowWordPainAura) then
                     shadowWordPainAura = MC.GetUnitAuraByName(npc, "Shadow Word: Pain");
                 end
-                if (not shadowWordPainAura) then
+                if (not shadowWordPainAura and MC.IsCastable("暗言术：痛", nil, npc)) then
                     MC.Cast("暗言术：痛", nil, npc);
                     ResetAfkTimer();
                     return;
